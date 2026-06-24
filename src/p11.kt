1ff0e4cc104896c68dd689296648860944a99c6f
Rutul Patel <rutulpatel5407@gmail.com>
2026-06-24 11:44:20 +0530

    Pr 1.1

---
+++ src/p11.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var j: Int = 22
+    var f: Float = 22.5f
+    var c: Char = 'T'
+    var s: String = "Rutul"
+    var b: Boolean = true
+    var d: Double = 3.14
+    var l: Long = 24012011123
+    var sh: Short = -2
+    var by: Byte = 127
+
+    println("Integer Value: $j")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $s")
+    println("Boolean Value: $b")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $sh")
+    println("Byte Value: $by")
+
+    println("Sum of all:${j+f}")
+}
